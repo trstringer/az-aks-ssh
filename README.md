039a60efa1fb474740c4965400bcdf6c6c08abaf
Thomas Stringer <thstring@microsoft.com>
2021-04-18 14:03:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# SSH into AKS agent nodes
+
+There is documentation on [how to SSH into AKS nodes (Microsoft docs)](https://docs.microsoft.com/en-us/azure/aks/ssh). That process is long and quite manual. This script automates this the connection.
+
+*Note: this script is currently in alpha and should not be used in a production environment.*
+
+## Usage
+
+```
+Usage:
+  SSH into an AKS agent node (pass in -c to run a single command
+  or omit for an interactive session):
+    ./az-aks-ssh.sh \
+        -g|--resource-group <resource_group> \
+        -n|--cluster-name <cluster> \
+        -d|--node-name <node_name|any> \
+        [-c|--command <command>] \
+        [-o|--output-file <file>]
+
+  Delete all locally generated SSH keys (~/.ssh/az_aks_*):
+    ./az-aks-ssh.sh --clear-local-ssh-keys
+
+  Delete the SSH proxy pod:
+    ./az-aks-ssh.sh --delete-ssh-pod
+
+  Cleanup SSH (delete SSH proxy pod and remove all keys):
+    ./az-aks-ssh.sh --cleanup
+```
+
+## Examples
+
+**SSH into any agent node in an interactive SSH session**
+
+```
+$ ./az-aks-ssh.sh -g rg1 -n aks1 -d any
+```
+
+**SSH into a specific agent node (get node name from `kubectl get no`)**
+
+```
+$ ./az-aks-ssh.sh -g rg1 -n aks1 -d cluster_node
+
+**Run a single command non-interactively**
+
+```
+$ ./az-aks-ssh.sh -g rg1 -n aks1 -d any -c "ps -aux"
+```
+
+**Run a command non-interactively and save the output to a file**
+
+```
+$ ./az-aks-ssh.sh -g rg1 -n aks1 -d any -c "ps -aux" -o ~/aks-ssh.out
+```
+
+**Cleanup the environment (delete agent node SSH keys locally and remove the SSH proxy pod)**
+
+```
+$ ./az-aks-ssh.sh --cleanup
+```
+
+## More information
+
+**Design**
+
+![Design](./design.png)
+
+**SSH keys**
+
+The SSH keys are generated for individual nodes. This ensures that keys are not being reused for multiple hosts. `--cleanup` removes all keys that match the prefix: `~/.ssh/az_aks*`.
+
+**SSH proxy pod**
+
+This design uses a proxy pod that sleeps forever so that it can be reused. `--cleanup` deletes this pod from the Kubernetes cluster. To see this pod you can run `kubectl get po aks-ssh-session`.
---
+++ az-aks-ssh.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+
+set -euo pipefail
+
+COMMAND=""
+OUTPUT_FILE="/dev/stdout"
+CLEAR_LOCAL_KEYS=""
+DELETE_SSH_POD=""
+SSH_POD_NAME="aks-ssh-session"
+CLEANUP=""
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage:"
+    echo "  SSH into an AKS agent node (pass in -c to run a single command"
+    echo "  or omit for an interactive session):"
+    echo "    ./az-aks-ssh.sh \\"
+    echo "        -g|--resource-group <resource_group> \\"
+    echo "        -n|--cluster-name <cluster> \\"
+    echo "        -d|--node-name <node_name|any> \\"
+    echo "        [-c|--command <command>] \\"
+    echo "        [-o|--output-file <file>]"
+    echo ""
+    echo "  Delete all locally generated SSH keys (~/.ssh/az_aks_*):"
+    echo "    ./az-aks-ssh.sh --clear-local-ssh-keys"
+    echo ""
+    echo "  Delete the SSH proxy pod:"
+    echo "    ./az-aks-ssh.sh --delete-ssh-pod"
+    echo ""
+    echo "  Cleanup SSH (delete SSH proxy pod and remove all keys):"
+    echo "    ./az-aks-ssh.sh --cleanup"
+    exit
+fi
+
+while [[ $# -gt 0 ]]; do
+    ARG="$1"
+
+    case $ARG in
+        -g|--resource-group)
+            RESOURCE_GROUP="$2"
+            shift
+            shift
+            ;;
+        -n|--cluster-name)
+            CLUSTER="$2"
+            shift
+            shift
+            ;;
+        -d|--node-name)
+            NODE_NAME="$2"
+            shift
+            shift
+            ;;
+        -c|--command)
+            COMMAND="$2"
+            shift
+            shift
+            ;;
+        -o|--output-file)
+            OUTPUT_FILE="$2"
+            shift
+            shift
+            ;;
+        --clear-local-ssh-keys)
+            CLEAR_LOCAL_KEYS="yes"
+            shift
+            ;;
+        --delete-ssh-pod)
+            DELETE_SSH_POD="yes"
+            shift
+            ;;
+        --cleanup)
+            CLEANUP="yes"
+            shift
+            ;;
+    esac
+done
+
+clear_local_keys () {
+    echo "Clearing local keys"
+    rm ~/.ssh/aks_ssh_*
+}
+
+delete_ssh_pod () {
+    echo "Deleting SSH pod $SSH_POD_NAME"
+    kubectl delete po "$SSH_POD_NAME"
+}
+
+if [[ -n "$CLEAR_LOCAL_KEYS" ]]; then
+    clear_local_keys
+    exit
+fi
+
+if [[ -n "$DELETE_SSH_POD" ]]; then
+    delete_ssh_pod
+    exit
+fi
+
+if [[ -n "$CLEANUP" ]]; then
+    clear_local_keys
+    delete_ssh_pod
+    exit
+fi
+
+if [[ "$NODE_NAME" == "any" ]]; then
+    echo "Selected 'any' node name, getting the first node"
+    NODE_NAME=$(kubectl get no -o jsonpath="{.items[0].metadata.labels['kubernetes\.io/hostname']}")
+fi
+
+echo "Using node: $NODE_NAME"
+
+NODE_RESOURCE_GROUP=$(az aks show \
+    --resource-group "$RESOURCE_GROUP" \
+    --name "$CLUSTER" \
+    --query "nodeResourceGroup" -o tsv \
+    --only-show-errors)
+VMSS_LIST=$(az vmss list \
+    --resource-group "$NODE_RESOURCE_GROUP" \
+    --query "[*].name" -o tsv \
+    --only-show-errors)
+echo "Found VMSS(es):"
+echo "$VMSS_LIST"
+CONTAINING_VMSS=""
+for VMSS in $VMSS_LIST; do
+    INSTANCE_ID=$(az vmss list-instances \
+        --resource-group "$NODE_RESOURCE_GROUP" \
+        --name "$VMSS" \
+        --query "[?osProfile.computerName == '$NODE_NAME'].{instanceId:instanceId}" -o tsv)
+    if [[ -n "$INSTANCE_ID" ]]; then
+        CONTAINING_VMSS="$VMSS"
+        break
+    fi
+done
+
+if [[ -z "$CONTAINING_VMSS" ]]; then
+    echo "Unable to locate node $NODE_NAME in any VMSS"
+    exit 1
+else
+    echo "Found $NODE_NAME in $CONTAINING_VMSS"
+fi
+
+SSH_KEY_FILE_NAME="aks_ssh_${NODE_NAME}"
+SSH_KEY_FILE="~/.ssh/${SSH_KEY_FILE_NAME}"
+SSH_KEY_FILE="${SSH_KEY_FILE/#\~/$HOME}"
+CREATED_KEY_FILE=""
+if [[ ! -f "$SSH_KEY_FILE" ]]; then
+    echo "Key doesn't exist. Creating new key: $SSH_KEY_FILE"
+    ssh-keygen \
+        -t rsa \
+        -N "" \
+        -q -f "$SSH_KEY_FILE"
+    CREATED_KEY_FILE="yes"
+fi
+
+echo "Instance ID is $INSTANCE_ID"
+
+ACCESS_EXTENSION=$(az vmss show \
+    --resource-group "$NODE_RESOURCE_GROUP" \
+    --name "$CONTAINING_VMSS" \
+    --instance-id $INSTANCE_ID \
+    --query "instanceView.extensions[?name == 'VMAccessForLinux']" -o tsv)
+
+if [[ -z "$ACCESS_EXTENSION" || -n "$CREATED_KEY_FILE" ]]; then
+    echo "Access extension does not exist or new key generated, adding to VM"
+    az vmss extension set \
+        --resource-group "$NODE_RESOURCE_GROUP" \
+        --vmss-name "$CONTAINING_VMSS" \
+        --name "VMAccessForLinux" \
+        --publisher "Microsoft.OSTCExtensions" \
+        --version "1.4" \
+        --protected-settings "{\"username\":\"azureuser\", \"ssh_key\":\"$(cat ${SSH_KEY_FILE}.pub)\"}" > /dev/null
+
+    az vmss update-instances \
+        --resource-group "$NODE_RESOURCE_GROUP" \
+        --name "$CONTAINING_VMSS" \
+        --instance-ids "$INSTANCE_ID"
+else
+    echo "Access extension already exists"
+fi
+
+INSTANCE_IP=$(kubectl get no \
+    "$NODE_NAME" \
+    -o jsonpath="{.status.addresses[?(@.type == 'InternalIP')].address}")
+
+echo "Instance IP is $INSTANCE_IP"
+
+if ! kubectl get po "$SSH_POD_NAME"; then
+    echo "Proxy pod doesn't exist, setting it up"
+    kubectl run "$SSH_POD_NAME" --image ubuntu:bionic -- /bin/bash -c "sleep infinity"
+    while true; do
+        echo "Waiting for proxy pod to be in a Running state"
+        POD_STATE=$(kubectl get po "$SSH_POD_NAME" -o jsonpath="{.status.phase}")
+        if [[ "$POD_STATE" == "Running" ]]; then
+            break
+        fi
+        sleep 1
+    done
+    kubectl exec "$SSH_POD_NAME" -- /bin/bash -c "apt-get update && apt-get install -y openssh-client"
+fi
+
+kubectl cp "$SSH_KEY_FILE" "${SSH_POD_NAME}:/${SSH_KEY_FILE_NAME}"
+kubectl exec "$SSH_POD_NAME" -- chmod 400 /$SSH_KEY_FILE_NAME
+
+if [[ -z "$COMMAND" ]]; then
+    echo "No command passed, running in interactive mode"
+    kubectl exec -it "$SSH_POD_NAME" -- /bin/bash -c "ssh -i /$SSH_KEY_FILE_NAME -o StrictHostKeyChecking=no azureuser@$INSTANCE_IP"
+else
+    echo "Running command non-interactively"
+    kubectl exec "$SSH_POD_NAME" -- /bin/bash -c "ssh -i /$SSH_KEY_FILE_NAME -o StrictHostKeyChecking=no azureuser@$INSTANCE_IP '$COMMAND'" > "$OUTPUT_FILE"
+fi
